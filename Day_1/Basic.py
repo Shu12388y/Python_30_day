e40bc09c5886cb9136e37fdd2b48ce032a1a9ce8
Shu12388y <spl.sp999@gmail.com>
2023-07-25 18:30:14 +0530

    basic of pyhton is add

---
+++ Day_1/Basic.py
@@ -0,0 +1,30 @@
+# how to print a statement
+
+# //?we have to use print() function
+
+# print("hello world")
+
+
+# How to take input from the user
+# //?we have to use input() function
+
+# input("enter a name")
+
+
+# pratice
+# //? we have to make greeting functionality
+
+
+
+name=input("Enter your name ")
+print("Hello",name,"nice to see you")
+
+# //! DONE
+
+
+# //? let us define some data type
+
+number=12   # it is integer data type
+float=9.87  # it is floating data type
+string="I am string"  # It is string data type
+boolean=True   #It is boolean data type
