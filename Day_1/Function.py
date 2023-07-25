4eeed64b220fe5bc110c96b41d82b4b9edee3051
Shu12388y <spl.sp999@gmail.com>
2023-07-25 18:47:45 +0530

    file are added

---
+++ Day_1/Function.py
@@ -0,0 +1,26 @@
+# //? we will see how to create a function and how to use it
+# //? For creating a function we have use following structure
+# //? def function_name(arg1,arg2):
+# //? function logic 
+# //? call the function
+
+
+# def add_number(num1,num2):
+#     sum=num1+num2
+#     print("sum of the number is ",sum)
+
+
+
+
+# add_number(2,5)  
+# //?Output : sum of the number is  7
+
+
+# //? TODO: create a function which take a number as argument and print "apple" if the number is even and divisible by 3 and print "orange" if the number is divisible by 5
+
+def check_number(num):
+     for element in num:
+        print(element)
+
+
+check_number(2)
---
+++ Day_1/Loop.py
@@ -0,0 +1,28 @@
+# //? we will use how to loop a string or how to use a loop
+
+
+# for loop
+languages = ['Python', 'Go', 'JavaScript']
+# for loop
+for i in languages:
+    print(i)
+
+
+
+# //? we will use while loop
+
+i = 1
+n = 5
+
+# while loop from i = 1 to 5
+while i <= n:
+    print(i)
+    i = i + 1    
+
+# output:
+# 1
+# 2
+# 3
+# 4
+# 5    
+
