19dda0222ea4a9a6e285e8387cf5c5fa96b22868
Shu12388y <spl.sp999@gmail.com>
2023-09-15 18:14:41 +0530

    list folder added

---
+++ Lists/app.py
@@ -0,0 +1,12 @@
+# list is used to store the multiple data in a single variable
+
+friends=["shubham","kunal","abhishek","sorave","deepak","arya","anshu"] 
+# this are list of friends
+# to access it we have to use index position of the list
+
+# it will print the firsr element
+print(friends[0])
+
+
+# it will print from the second element to the last element 
+print(friends[1:])
