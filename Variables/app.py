28df79687acaa13214e808311f977f35ac0a15f5
Shu12388y <spl.sp999@gmail.com>
2023-09-15 17:26:34 +0530

    variable are added

---
+++ Variables/app.py
@@ -0,0 +1,6 @@
+# variable used to store the data inside it
+# To create a variable
+a=54  # we have created a variable that store 54
+name="shubham paul" # It store a string data type
+decimal=3.12 # It store floating value 
+bool=True # It store boolean value
